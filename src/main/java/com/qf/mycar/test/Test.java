a3957a64f0bd2d2fa567b8c13240f7257b98bf72
wupengfeishiro <1930369489@qq.com>
2019-10-13 13:32:20 +0800

    创建项目

---
+++ src/main/java/com/qf/mycar/test/Test.java
@@ -0,0 +1,10 @@
+package com.qf.mycar.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            System.out.println("最初版本"+i);
+        }
+    }
+}
