f9595989edf29d696c03f6ea9e5534aba47b5e9d
wupengfeishiro <1930369489@qq.com>
2019-10-13 15:23:39 +0800

    mine

--- src/main/java/com/qf/mycar/test/Test.java
+++ src/main/java/com/qf/mycar/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         for (int i=0;i<5;i++){
-
+            System.out.println("最初版本"+i+"这里我改的才对");
+            System.out.println("我是后提交的，要墨汁");
         }
     }
 }
