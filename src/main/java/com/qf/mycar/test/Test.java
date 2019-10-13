a429ec661207dffa2c0fb098c0d52381ffc5273e
wupengfeishiro <1930369489@qq.com>
2019-10-13 14:09:35 +0800

    在你提交之前我先提交

--- src/main/java/com/qf/mycar/test/Test.java
+++ src/main/java/com/qf/mycar/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         for (int i=0;i<5;i++){
-            System.out.println("最初版本"+i);
+            System.out.println("最初版本"+i+"在你提交之前我先提交了");
+            System.out.println("在你提交之前我先提交了");
         }
     }
 }
