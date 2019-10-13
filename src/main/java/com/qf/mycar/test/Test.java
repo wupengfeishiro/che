872a36499c6b7ad773995f5b7fa21325154380ce
wupengfeishiro <1930369489@qq.com>
2019-10-13 14:42:12 +0800

    先提交

--- src/main/java/com/qf/mycar/test/Test.java
+++ src/main/java/com/qf/mycar/test/Test.java
@@ -4,8 +4,12 @@ public class Test {
 
     public static void main(String[] args) {
         for (int i=0;i<5;i++){
-            System.out.println("最初版本"+i+"这里我改的才对");
+            System.out.println("最初版本"+i);
             System.out.println("我是后提交的，要墨汁");
         }
+    }
+
+    public void test(){
+
     }
 }
